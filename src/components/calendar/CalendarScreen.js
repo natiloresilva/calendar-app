107cdf953105920aef26e37063a76cb6342e419a
Natalia <natalia.pupushi@gmail.com>
2021-10-31 22:02:11 +0100

    added style & events props to calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import { Navbar } from '../ui/Navbar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-
+import { CalendarEvent } from './CalendarEvent'
 
 const localizer = momentLocalizer(moment)
 
@@ -11,10 +11,45 @@ const events = [{
     title: 'Cumpleaños de Lara',
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Galileo'
+    }
 }]
 
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month')
+
+    const onDoubleClick = (e) => {
+        console.log('double')
+    }
+
+    const onSelectEvent = (e) => {
+        console.log('onSelect')
+    }
+
+    const onViewChange = (e) => {
+        setLastView(e)
+        localStorage.setItem('lastView', e)
+    }
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <div className="calendar-screen">
             <Navbar />
@@ -24,6 +59,14 @@ export const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
+                eventPropGetter={eventStyleGetter}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelectEvent}
+                onView={onViewChange}
+                view={lastView}
+                components={{
+                    event: CalendarEvent
+                }}
             />
         </div>
     )
